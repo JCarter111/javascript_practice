482c9722568e93e178f7885bfd49265430fabb27
Jackie Carter <jacqueline.carter@hotmail.co.uk>
2019-04-12 17:09:59 +0100

    Completed Week4 challenges using forEach

--- challenges/week4.js
+++ challenges/week4.js
@@ -3,17 +3,17 @@ function findSmallNums(nums) {
   // Your code here
   // return any numbers in the nums array
   // that are less than 1 e.g. decimals or negative numbers
-  // use for loop to test each number in nums
+  // use forEach statements
   // if statement to test whether each number less than 1
   // returned numbers appear in the same order that they
   // appeared in in the nums array
   // define a return array to store the small numbers
   const smallNums = [];
-  for (let i = 0; i < nums.length; i++) {
+  nums.forEach(function(numberItem) {
     // if a small number (<1) is located add
     // the number to the return array
-    if (nums[i] < 1) smallNums.push(nums[i]);
-  }
+    if (numberItem < 1) smallNums.push(numberItem);
+  });
   // return the small numbers array
   return smallNums;
 }
@@ -24,7 +24,7 @@ function findNamesBeginningWith(names, char) {
   // Your code here
   // return any names in names where the first letter in the
   // item matches char
-  // use for loop to check the first character of each
+  // use forEach check the first character of each
   // name in the names array
   // returned names appear in the same order as they appear 
   // in the names array
@@ -35,11 +35,11 @@ function findNamesBeginningWith(names, char) {
   // be returned. 
   // define return array to store matched names
   const matchNames = [];
-  for (let i = 0; i < names.length; i++) {
+  names.forEach (function(nameItem) {
     // if the current name begins with char add the
     // name to the return array
-    if (names[i].charAt(0) === char) matchNames.push(names[i]);
-  }
+    if (nameItem.charAt(0) === char) matchNames.push(nameItem);
+  });
   // return array of names with first letter matching char
   return matchNames;
 }
@@ -51,7 +51,7 @@ function findVerbs(words) {
   // with "to" 
   // Note: "to" in the first two characters of the string
   // only
-  // use for loop to check the first 2 characters in each
+  // use forEach to check the first 2 characters in each
   // string in the words array
   // returned strings appear in the same order as they appear
   // in the words array
@@ -64,10 +64,10 @@ function findVerbs(words) {
   // search for "to " to avoid selecting strings starting 
   // with a first word such as "touch"
   const verbsArr = [];
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].startsWith("to ")) verbsArr.push(words[i]);
+  words.forEach(function(wordItem) {
+    if (wordItem.startsWith("to ")) verbsArr.push(wordItem);
     //if (words[i].slice(0,3) === "to ") verbsArr.push(words[i]);
-  }
+  });
   // return array of verbs
   return verbsArr;
 }
@@ -82,13 +82,13 @@ function getIntegers(nums) {
   // array.
   // Note: no sorting of the integers is required
   // need a test to check number in nums is an integer
-  // use for loops
+  // use forEach to loop through array of numbers
   const intArr = [];
-  for (let i = 0; i < nums.length; i++) {
+  nums.forEach(function(numsItem) {
     // if an integer is located in nums[i]
     // add the integer to the return array
-    if (Number.isInteger(nums[i])) intArr.push(nums[i]);
-  }
+    if (Number.isInteger(numsItem)) intArr.push(numsItem);
+  });
   // return array of integers
   return intArr;
 }
@@ -103,18 +103,18 @@ function getCities(users) {
   // Note: the city names are returned in the order
   // in which they appear in users i.e. not sorted
   // alphabetically
-  // use for loops to check each city in the 
+  // use forEach to check each city in the 
   // array - need to use data.city.displayName for 
   // each user
   // of user objects
   // create a return array to store city display names
   const cityDispNameArr = [];
-  for (let i = 0; i < users.length; i++) {
+  users.forEach(function(userItem) {
     // check that displayName exists
-    if (users[i].data.city.hasOwnProperty("displayName")) {
-      cityDispNameArr.push(users[i].data.city.displayName);
+    if (userItem.data.city.hasOwnProperty("displayName")) {
+      cityDispNameArr.push(userItem.data.city.displayName);
     }
-  }
+  });
   // return the array of city display names
   return cityDispNameArr;
 }
@@ -126,18 +126,18 @@ function getSquareRoots(nums) {
   // square root of each number in the nums array
   // Math.sqrt - clearer in the code
   // round the results to 2 decimal places
-  // use for loop to loop through nums array
+  // use a forEach to loop through nums array
   // Note: assuming that nums only has positive
   // numbers - square root functions above would
   // return NaN for a negative number
   const sqrRootArr = [];
   let sqrRootNumber = 0;
-  for (let i = 0; i < nums.length; i++) {
+  nums.forEach(function (numsItem) {
     // find the square root of the current number in the array
     // round the square root to two decimal places
-    sqrRootNumber = Math.round(Math.sqrt(nums[i]) * 100) / 100;
+    sqrRootNumber = Math.round(Math.sqrt(numsItem) * 100) / 100;
     sqrRootArr.push(sqrRootNumber);
-  }
+  });
   // return the square root array
   return sqrRootArr;
 }
@@ -150,7 +150,7 @@ function findSentencesContaining(sentences, str) {
   // containing the specified text
   // search for presence of string in array element
   // search must not be case sensitive
-  // use for loops  
+  // use forEach to loop through the sentences array 
   // for each element in the sentences array, split the
   // string into each word, convert each word to lowercase
   // for non case sensitive matching with str
@@ -160,15 +160,16 @@ function findSentencesContaining(sentences, str) {
   // create return array of strings which contain str
   const containsStrArr = [];
 
-  for (let i = 0; i < sentences.length; i++) {
+  sentences.forEach(function(sentenceItem) {
     // convert each word in the sentence to lower case 
     // for non case sensitive matching
     // if str is located in the current sentence add the
     // sentence to the return array
-    if (sentences[i].toLowerCase().includes(str)) {
-      containsStrArr.push(sentences[i]);
+    // use forEach to loop through the array
+    if (sentenceItem.toLowerCase().includes(str)) {
+      containsStrArr.push(sentenceItem);
     }
-  }
+  });
   // return the array of sentences with str in them
   return containsStrArr;
 }
@@ -178,30 +179,30 @@ function getLongestSides(triangles) {
   // Your code here
   // for each triangle in the triangles array
   // determine the longest side
-  // use for loops to find the longest sides
-  // nested for loops - one loop for triangles array
-  // one loop for the arrays of sides for each 
+  // use nest forEach loops to find the longest sides
+  // nested forEach - one forEach for triangles array
+  // second forEach for the arrays of sides for each 
   // triangle
   // create a return array to store the longest side
   // of each triangle
   const longestSides = [];
   // loop through each triangle in the triangles
   // array
-  for (let i = 0; i < triangles.length; i++) {
+  triangles.forEach (function (sideArr) {
     // create a number to store the longest side
     // for each triangle
     let longestSide = 0;
     // reset the triangle sides array to the 
     // array of sides for the current triangle
-    for (let j = 0; j < triangles[i].length; j++) {
-      if (triangles[i][j] > longestSide) {
-        longestSide = triangles[i][j];
+    sideArr.forEach (function (sideItem) {
+      if (sideItem > longestSide) {
+        longestSide = sideItem;
       }
-    }
+    });
     // add the longest side of the current triangle
     // to the return array
     longestSides.push(longestSide);
-  }
+  });
   // return the array of longestSides
   return longestSides;
 }
